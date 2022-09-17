40bf4a2d1e3de7fac37249c39f0527d907ad999c
jadakedak <58665733+jadakedak@users.noreply.github.com>
2022-09-17 13:01:26 +0200

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,46 @@
+import keyboard as kb
+import random
+
+def writeWords():
+    hotkey = input("\033[1;35;40m[+]\033[+] choose a hotkey [default: R]: ")
+    if hotkey:
+        print(f"\033[1;31;40m[+]\033[+] all set just press '{hotkey.upper()}' when ready!")
+        with open("wordlist.txt", "r") as file:
+            for word in file:
+                kb.wait(hotkey)
+                kb.write(word)
+                kb.press("Enter")
+    elif hotkey == "":
+        print(f"\033[1;31;40m[+]\033[+] all set just press 'R' when ready!")
+        with open("wordlist.txt", "r") as file:
+            for word in file:
+                kb.wait("r")
+                kb.write(word)
+                kb.press("Enter")
+    else:
+        print(f"[+] {hotkey} is an invalid hotkey!")
+
+
+def wordGen():
+    number_of_words = int(input("\033[1;33;40m[+]\033[+] how many lines to generate?: "))
+    letters_to_add = int(input("\033[1;33;40m[+]\033[+] how many letters for each line?: "))
+    print("\033[1;31;40m\033[+] generating words...")
+    num = 0
+    word = ""
+    chars = "abcdefghijklmnopqrstuvxyz!#%&/()=?@$€{[]}|`+ABCDEFGHIJKLMNOPQRSTUVXYZ"
+    while num <= number_of_words: 
+        for i in range(0,letters_to_add):
+            word += random.choice(chars)
+        with open("wordlist.txt", "a") as Wlist:
+            Wlist.write(word)
+            Wlist.write("\n")
+            num += 1
+            word = ""
+    print("[+] words added!")
+    writeWords(2, 0)
+
+        
+
+
+if __name__ == "__main__":
+    wordGen()
